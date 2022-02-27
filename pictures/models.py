458ae545ee8e51f261ba883324d4375f6f6d4eb8
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-02-27 22:52:22 +0300

    created image model and added save and delete functions

--- pictures/models.py
+++ pictures/models.py
@@ -36,7 +36,17 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=60)
     description = models.TextField()
-    author = models.CharField(max_length=40, default='admin')
-    date = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category)
-    location = models.ForeignKey(Location)        
+    # author = models.CharField(max_length=40, default='admin')
+    # date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)  
+    
+    
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()      
