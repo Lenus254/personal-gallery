51f3ae22a1c6b562e2ce0b0e6084467373933986
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-02-27 22:46:40 +0300

    created class category and added save, delete functions

--- pictures/models.py
+++ pictures/models.py
@@ -16,3 +16,27 @@ class Location(models.Model):
     def update_location(self, name): 
         self.name=name
         self.save   
+        
+        
+        
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+        
+        
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    author = models.CharField(max_length=40, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)        
