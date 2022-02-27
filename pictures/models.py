6901ce6587cf512fb0f2636c0bfc68de7402c657
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-02-27 16:27:49 +0300

    added a function for saving location

--- pictures/models.py
+++ pictures/models.py
@@ -4,3 +4,6 @@ from django.db import models
 # Create your models here.
 class Location(models.Model):
     name = models.CharField(max_length=60)
+    
+    def save_location(self):
+        self.save()
