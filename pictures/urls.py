86318dd4052b4586faa2703a2ac548a35368dadf
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-02-28 23:04:56 +0300

    modified search url pattern

--- pictures/urls.py
+++ pictures/urls.py
@@ -7,7 +7,7 @@ app_name = 'pictures'
 
 urlpatterns = [
    path('', views.index, name='index'),
-   path('search/', views.search_results, name='search'),
+   path('search/', views.search_results, nme='search'),
 ]
 
 if settings.DEBUG:
