b110c788b2a0e619b6425e99113d833726b319f0
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-03-01 05:53:23 +0300

    added a test case for testing if image is correctly saved

--- pictures/tests.py
+++ pictures/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Image, Category, Location
+
+
+class TestImage(TestCase):
+    def setUp(self):
+        self.location = Location(name='Mara')
+        self.location.save_location()
+
+        self.category = Category(name='travel')
+        self.category.save_category()
+
+        self.image_test = Image(id=1, name='image', description='this is a test image', location=self.location,
+                                category=self.category)
